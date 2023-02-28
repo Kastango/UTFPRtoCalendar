4327413701d38d91b36209effacfdbc9802076c0
Kastango <Metroidling@gmail.com>
2023-02-28 00:53:36 -0300

    added styling stuff

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,6 +17,34 @@ module.exports = {
         'true-yellow': '#FBBC04',
         'almost-black': 'rgba(31,31,31,1.0)'
       },
+      keyframes: {
+        headShake: {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '6.5%': {
+            transform: 'translateX(-2px) rotateY(-2deg)',
+          },
+
+          '18.5%': {
+            transform: 'translateX(3px) rotateY(3deg)',
+          },
+
+          '31.5%': {
+            transform: 'translateX(0px) rotateY(0deg)',
+          },
+
+          '43.5%': {
+            transform: 'translateX(2px) rotateY(2deg)',
+          },
+          '50%': {
+            transform: 'translateX(0)',
+          },
+        },
+      },
+      animation: {
+        headShake: 'headShake 2s infinite',
+      },
     },
   },
   plugins: [
